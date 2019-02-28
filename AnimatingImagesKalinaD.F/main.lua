8aebffc27d59a11ffbef1682d24daf5c3ac00b3e
Kalina Dunne Farrell <kalina.dunne-farrell@ocsbstudent.ca>
2019-02-28 09:35:28 -0500

    Updated AnimatingImages

--- AnimatingImagesKalinaD.F/main.lua
+++ AnimatingImagesKalinaD.F/main.lua
@@ -1,7 +1,43 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: AnimatingImages
+-- Name: Kalina Dunne Farrell
+-- Course: ICS2O
+-- This pogram displays 3+ images moving in different directions, with at least one image
+-- moving in a diagonal line and at least one ofther growing/shrink or fading in/out.
 -----------------------------------------------------------------------------------------
+-- setup
 
--- Your code here
+-- hide the statusbar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set the background
+local backgroundImage = display.newImageRect("Images/multiColorBackground.png", 2028, 1530)
+-----------------------------------------------------------------------------------------
+-- character #1 (hummingbird)
+
+-- character image with width and height
+local hummingbird = display.newImageRect("Images/hummingbird.png", 200, 200)
+
+-- set the initial (x,y) positions of hummingbird
+hummingbird.x = 800
+hummingbird.y = 600
+
+-- set the scrollSpeed for the x and y values of hummingbird
+scrollSpeedXBird = -2
+scrollSpeedYBird = -2
+
+-- Function: MoveBird
+-- Input: this funcion accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x and y value of hummingbird
+local function MoveBird(event)
+	-- add the scroll speed to the x-value of hummingbird
+	hummingbird.x = hummingbird.x + scrollSpeedXBird
+	-- add the scroll speed to the y-value of hummingbird
+	hummingbird.y = hummingbird.y + scrollSpeedYBird
+end
+
+-- MoveBird will be called over and over again
+Runtime:addEventListener("enterFrame", MoveBird)
+-----------------------------------------------------------------------------------------
+-- character
